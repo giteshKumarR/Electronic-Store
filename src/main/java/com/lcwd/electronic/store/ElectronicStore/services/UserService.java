df2a8a84d4bb0585a5d5baa41b880fa2d0994288
Gitesh Kumar <gitesh.kumar2503@gmail>
2024-12-07 19:35:11 +0530

    Added User Repository

---
+++ src/main/java/com/lcwd/electronic/store/ElectronicStore/services/UserService.java
@@ -0,0 +1,14 @@
+package com.lcwd.electronic.store.ElectronicStore.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserService {
+    // Create User
+    // Update User
+    // Get All Users
+    // Get single user by Id
+    // Get single user by email
+    // Search User
+    // Other User Specific Service Methods
+}
