7ceaf25d1ee6c8c76de0907a6a12f27e4427b0ec
Gitesh Kumar <gitesh.kumar2503@gmail>
2024-12-08 23:45:07 +0530

    Added Bean vlaidation(Hibernate Bean validation concepts into the userDTO and contrller)

--- src/main/java/com/lcwd/electronic/store/ElectronicStore/controllers/UserController.java
+++ src/main/java/com/lcwd/electronic/store/ElectronicStore/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.lcwd.electronic.store.ElectronicStore.controllers;
 import com.lcwd.electronic.store.ElectronicStore.dtos.UserDto;
 import com.lcwd.electronic.store.ElectronicStore.payload.ApiResponseMessage;
 import com.lcwd.electronic.store.ElectronicStore.services.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,13 +19,13 @@ public class UserController {
 
     // create
     @PostMapping("/create-user")
-    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
+    public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto userDto) {
         return new ResponseEntity<>(userService.createUser(userDto), HttpStatus.CREATED);
     }
 
     // update
     @PutMapping("/update-user/{userId}")
-    public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userWithUpdatedDetails,
+    public ResponseEntity<UserDto> updateUser(@Valid @RequestBody UserDto userWithUpdatedDetails,
                                               @PathVariable String userId) {
         return new ResponseEntity<>(userService.updateUser(userWithUpdatedDetails, userId), HttpStatus.OK);
 
--- src/main/java/com/lcwd/electronic/store/ElectronicStore/dtos/UserDto.java
+++ src/main/java/com/lcwd/electronic/store/ElectronicStore/dtos/UserDto.java
@@ -1,5 +1,9 @@
 package com.lcwd.electronic.store.ElectronicStore.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -9,11 +13,24 @@ import lombok.*;
 @Builder
 public class UserDto {
     private String userId;
+
+    @NotBlank(message = "Specify whether user Active or Not !!")
+    @Size(min = 6, max = 8, message = "Invalid Status !!")
     private String UserStatus;
+
+    @Size(min = 3, max=15, message="Invalid Name !!")
     private String userName;
+
+    @Email(message = "Invalid User Email !!")
     private String userEmail;
+
+    @NotBlank(message = "Password is required !!")
     private String userPassword;
+
+    @Size(max = 1, message = "Invalid gender !!")
     private String userGender;
+
+    @NotBlank(message = "Write Something about yourself !!")
     private String userAbout;
     private String userProfileImage;
 }
