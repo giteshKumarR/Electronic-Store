6438da6a7d56a04118e7b679a46f66f35a7bd593
Gitesh Kumar <gitesh.kumar2503@gmail>
2024-12-27 13:18:51 +0530

    BUG FIX: fixed validation bug on Address, her ewe cannot apply on Address and other enums.

--- src/main/java/com/lcwd/electronic/store/ElectronicStore/payload/orderpayload/OrderRequest.java
+++ src/main/java/com/lcwd/electronic/store/ElectronicStore/payload/orderpayload/OrderRequest.java
@@ -14,13 +14,8 @@ import lombok.*;
 public class OrderRequest {
 //    private String cartId;  ---> I think it is not required as using the
     // userId only I can get the cart for the user.
-    @NotBlank(message = "Cart Id is required !!")
     private Address shippingAddress;
-
-    @NotBlank(message = "Billing address is required !!")
     private Address billingAddress;
-
-    @NotBlank(message = "Shipping Address is required !!")
     private ShippingMethod shippingMethod;
     private String paymentMethod;
     private String orderNotes;
